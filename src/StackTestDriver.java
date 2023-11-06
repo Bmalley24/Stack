c6e9efe8b9cc54441d56c214fb66fe083fe6aa86
Bmalley24 <bmalley24@kentdenver.org>
2023-11-06 14:44:40 -0700

    This is my completed moduals for Stack. I am now going to start work on my test driver.

--- src/StackTestDriver.java
+++ src/StackTestDriver.java
@@ -1,2 +1,12 @@
 public class StackTestDriver {
+    public static void main(String[] args) {
+
+        Stack testStack = new Stack();
+        System.out.println(testStack.peek());
+
+        testStack.push(88);
+        System.out.println(testStack.peek());
+    }
 }
+
+
