e0c9fff4b26fd066da6ff9ffb263855660b12dae
Bmalley24 <bmalley24@kentdenver.org>
2023-11-08 11:48:10 -0700

    I have finished my Test driver along with editing my push function so it would work correctly.

--- src/StackTestDriver.java
+++ src/StackTestDriver.java
@@ -1,11 +1,38 @@
 public class StackTestDriver {
     public static void main(String[] args) {
 
-        Stack testStack = new Stack();
-        System.out.println(testStack.peek());
+        Stack testStack = new Stack();//creates a new stack
 
-        testStack.push(88);
-        System.out.println(testStack.peek());
+        System.out.println(testStack.peek());//tests peek method when there is nothing inside
+        // (output: null)
+
+        testStack.push(88);//tests push method if it can correctly add on a new value
+
+        System.out.println(testStack.peek());//tests peek again to see if we have a new top value
+        // (output: 88)
+
+        testStack.push(27);//pushes on a new value to test if the stack can have more than 1 value
+
+        System.out.println(testStack.peek());//checks the new top value
+        // (output: 27)
+
+        System.out.println(testStack.size());//checks if size method works
+        // (output: 2)
+
+        System.out.println(testStack.pop());// checks if pop method returns removed value
+        // (output: 27)
+
+        System.out.println(testStack.peek());//checks if the pop method properly removed the old top value
+        // (output: 88)
+
+        System.out.println(testStack.isEmpty());//cheking if the isEmpty method works
+        // (output: flase)
+
+        System.out.println(testStack.pop());//removing the next top value to then check the isEmpty emthod a second time
+        // (output: 88)
+
+        System.out.println(testStack.isEmpty());//cheking if the isEmpty method works
+        // (output: true)
     }
 }
 
